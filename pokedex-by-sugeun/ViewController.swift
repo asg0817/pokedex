34c92f3a907f0390ab361752c84bd9479a56c9c0
G I <ig@igui-imach.local>
2016-02-01 10:45:28 +0900

    Initial Commit

---
+++ pokedex-by-sugeun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-sugeun
+//
+//  Created by IG on 2016. 2. 1..
+//  Copyright © 2016년 ansugeun.k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
